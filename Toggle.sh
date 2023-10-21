be3900eccb4e96ed5450a7d485ef3222cb3926e8
grrkekisbest <97322615+grrkekisbest@users.noreply.github.com>
2023-10-20 23:35:08 -0400

    Update Toggle.sh

--- Toggle.sh
+++ Toggle.sh
@@ -1,11 +1,11 @@
 !/bin/bash
 Id_Num=$(xsetwacom --list | grep touch | sed -r "s/.*id: *([0-9]*).*/\1/")
 x=$(xsetwacom --get $Id_Num touch)
-if [ $x = 'off' ]
+if [ '$x' = 'off' ]
 then
     xsetwacom --set $Id_Num touch on
     notify-send --hint int:transient:1 'Touch' 'Touch is now on'
-elif [ $x = 'on' ]
+elif [ '$x' = 'on' ]
 then
     xsetwacom --set $Id_Num touch off
     notify-send --hint int:transient:1 'Touch' 'Touch is now off'
